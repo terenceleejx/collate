0a1ee46e85d9eeaf8443f386dcb63ed05d8cde73
Terence Lee <terence.lee.jx@gmail.com>
2014-12-13 14:08:02 +0800

    added pagination to chart show

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,6 +2,6 @@ class TopicsController < ApplicationController
 	def show
 		## ignores lowercase
 		@topic = Topic.where("name ILIKE ?", "%#{params[:name]}%").take
-		@quantities = Quantity.joins(:topics).where(topics: { id: @topic.id }).order(date: :desc)
+		@quantities = Quantity.joins(:topics).where(topics: { id: @topic.id }).order(date: :desc).page params[:page]
 	end
 end
